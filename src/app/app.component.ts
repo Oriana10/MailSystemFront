916502d1d224400964b4e504eedf251740fe5979
Oriana Pellegrini <oriana.pellegrini@wiener-lab.com>
2024-01-10 20:20:09 -0300

    Add lgin inputs and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -13,4 +13,19 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent {
   title = 'mail-system';
+
+  showHead: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationEnd || event instanceof NavigationStart || event instanceof NavigationCancel || event instanceof NavigationError) {
+        if (event['url'] == '/login' || event['url'] == '/register') {
+          this.showHead = true;
+        } else {
+          this.showHead = false;
+        }
+      }
+    });
+  }
+  
 }
